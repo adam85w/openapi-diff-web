59c189c106faff595482a88ac2db2295287d2067
adam85w <adam85.w@gmail.com>
2025-12-01 01:00:27 +0100

    Added a rule to check for the existence of detached objects when the schemas are the same

--- src/main/java/net/adam85w/openapi/openapidiff/web/OpenApiDiffService.java
+++ src/main/java/net/adam85w/openapi/openapidiff/web/OpenApiDiffService.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.io.InputStreamSource;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Service
@@ -24,7 +25,9 @@ class OpenApiDiffService {
     ChangedOpenApi diff(InputStreamSource currentVersion, InputStreamSource newVersion, OpenApiFormat format) throws InvalidOpenAPIException {
         OpenAPI currentOpenApi = obtainOpenAPI(currentVersion, format).orElseThrow(() -> new InvalidOpenAPIException("Provided an invalid current version of the OpenAPI contract."));
         OpenAPI newOpenApi = obtainOpenAPI(newVersion, format).orElseThrow(() -> new InvalidOpenAPIException("Provided an invalid new version of the OpenAPI contract."));
-        return OpenApiCompare.fromSpecifications(currentOpenApi, newOpenApi);
+        ChangedOpenApi result = OpenApiCompare.fromSpecifications(currentOpenApi, newOpenApi);
+        validAgainstDetachedModel(result, currentVersion, newVersion);
+        return result;
     }
 
     protected Optional<OpenAPI> obtainOpenAPI(InputStreamSource inputStreamSource, OpenApiFormat format) throws InvalidOpenAPIException {
@@ -33,6 +36,17 @@ class OpenApiDiffService {
         } catch (Exception e) {
             LOGGER.warn("Error while reading OpenAPI from input stream", e);
             return Optional.empty();
+        }
+    }
+
+    protected void validAgainstDetachedModel(ChangedOpenApi result, InputStreamSource currentVersion, InputStreamSource newVersion) throws InvalidOpenAPIException {
+        try {
+            if (!result.isDifferent() && currentVersion.getInputStream().readAllBytes().length != newVersion.getInputStream().readAllBytes().length) {
+                throw new InvalidOpenAPIException("The versions match, but there is a size issue - probably one of the objects is detached from the model or something doesn’t comply with the OpenAPI specification.");
+            }
+        } catch (IOException e) {
+            LOGGER.warn("Error while reading OpenAPI from input stream", e);
+            throw new InvalidOpenAPIException("The versions match, but there is a size issue - probably one of the objects is detached from the model or something doesn’t comply with the OpenAPI specification.");
         }
     }
 }
